7d6fca82e0cc468ae8c6591f78291ec929b680b9
IvanildeSLacey <sobierai.ivanilde@gmail.com>
2022-11-29 08:32:00 +0000

    Add css for burger menu and html structure for main HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,34 @@
-const burger = document.querySelector(".burger");
-const navMenu = document.querySelector(".nav-menu");
+// Main Page carousel slide script 
+const carouselSlide = document.querySelectorAll(".img-container img");
 
-burger = addEventListener("click", () => {
-    burger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
+const timeDelay = 4000;
+let currentImageCounter = 0; // setting a variable to keep track of the current image (slide)
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-}))
+// carouselSlide[currentImageCounter].style.display = "block";
+carouselSlide[currentImageCounter].style.opacity = 1;
+
+setInterval(nextImage, timeDelay);
+
+function nextImage() {
+  // carouselSlide[currentImageCounter].style.display = "none";
+  carouselSlide[currentImageCounter].style.opacity = 0;
+
+  currentImageCounter = (currentImageCounter+1) % carouselSlide.length;
+
+  // carouselSlide[currentImageCounter].style.display = "block";
+  carouselSlide[currentImageCounter].style.opacity = 1;
+}
+
+
+// const burger = document.querySelector(".burger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// burger = addEventListener("click", () => {
+//     burger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     burger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }))
